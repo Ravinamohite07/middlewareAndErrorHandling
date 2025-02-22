82e754adf11ca3690e6a967fb0d65481778edc77
Ravinamohite07 <ravinamohite527@gmail.com>
2025-02-22 14:33:50 +0530

    wild-card error handling

---
+++ errorHandling/app01.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(err,req,res,next) => {
+    if (err) {
+        //log an error
+        console.log(err);
+        res.status(500).send("something wrong went..!");
+    }
+});
+
+app.get("/getUserData",(req,res) => {
+    try{
+        throw new error("xyzweee");
+        res.send("user data send..!");
+    }catch (err) {
+        res.status(500).send("some error contact support team..!");
+    }  
+});
+
+
+app.listen(3000,() => {
+    console.log("server succesfully run");
+});
---
+++ errorHandling/app03.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/getUserData",(req,res) => {
+    //try{
+        throw new error("xyzweee");
+        res.send("user data send..!");
+    //}catch (err) {
+        res.status(500).send("some error contact support team..!");
+    //}  
+});
+
+app.use("/",(err,req,res,next) => {
+    if (err) {
+        //log an error
+        console.log(err);
+        res.status(500).send("something wrong went..!");
+    }
+});
+
+
+
+app.listen(3000,() => {
+    console.log("server succesfully run");
+});
